8fa0466d7b7c27c1292f206be782cf5e838c4690
andy tutton <andytutton2@gmail.com>
2024-04-15 16:39:01 +0200

    maybe devising

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,23 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # resource :traveller, except: [:index] do
+  #   resources :booking, except: [:index]
+  # end
+
+  # resource :host, except: [:index] do
+  #   resources :booking, except: [:index]
+  # end
+
+
+  resources :experiences, only: [:show, :index]
+
+  # # Nested routes for hosts to manage experiences
+  # resources :host, except: [:index] do
+  #   resources :experiences, only: [:new, :create, :edit, :update, :destroy]
+  # end
+
+
   # Defines the root path route ("/")
   root to: "experiences#index"
 end
